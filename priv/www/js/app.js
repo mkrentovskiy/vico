e94cfa20883e5ac937ddd485d7e5c946c810bdb0
Max Krentovskiy <mkrentovskiy@gmail.com>
2013-02-01 23:33:27 +0400

    Still working on UI - Add broadcast.swf

--- priv/www/js/app.js
+++ priv/www/js/app.js
@@ -1,6 +1,14 @@
 (function($) {
 	$.app = {}
 	$.app.init = function() {
+		d = new Date();
+		swfobject.embedSWF(
+			"/swf/broadcast.swf?" + d.getTime(), 
+			"broadcast", 
+			$("#broadcast").width(), 
+			$("#broadcast").height(),
+			"11.0.0", 
+			{ server: "", stream: "" });
 	}
 
 })(window.jQuery);
